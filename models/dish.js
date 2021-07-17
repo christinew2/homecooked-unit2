559a10b634022737352dc734d0b31dae2c36c8c6
christinew2 <christinewei2@gmail.com>
2021-07-17 15:05:42 -0500

    create dish model

---
+++ models/dish.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+export {
+    Dish
+}
+
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+})
+
+const dishSchema = new Schema({
+    isActive: {
+        type: Boolean,
+        required: true,
+    },
+    isBuy: {
+        type: Boolean,
+        required: true,
+    },
+    dishName: {
+        type: String,
+        required: true,
+    },
+    minPrice: Number,
+    maxPrice: Number,
+    whoWants: [{type: Schema.Types.ObjectId, ref: "Profile"}],
+    comments: [commentSchema]
+
+}, {
+    timestamps: true,
+})
+
+const Dish = mongoose.model("Dish", dishSchema)
