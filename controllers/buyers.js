dd22754b158a3f4cfbe8efdbc187d0f7a6f350a4
christinew2 <christinewei2@gmail.com>
2021-07-17 16:55:57 -0500

    create buyers index page

--- controllers/buyers.js
+++ controllers/buyers.js
@@ -7,13 +7,14 @@ export {
 function index(req,res) {
     Dish.find({})
     .then (dishes => {
-        res.render("dishes/index", {
+        res.render("buyers/index", {
             dishes,
-            title: "HERE ARE THE DISHES"
+            title: "HERE ARE THE DISHES",
+            user: req.user ? req.user : null
         })
     })
     .catch (err => {
         console.log(err)
-        res.redirect("/dishes")
+        res.redirect("/buyers")
     })
 }
